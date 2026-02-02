a0871bb9ea7909ff88ca874ce2074a59194a1094
rikulpatel@gmail.com <rikulpatel@gmail.com>
2026-02-01 18:30:11 -0600

    test deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ dependencies {
     androidTestImplementation(libs.kotlinx.coroutines.testing)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.espresso.intents)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
